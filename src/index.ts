fcb1d75060ef7db14737c1dc024b9a72f1808799
Ying <473380917@qq.com>
2021-06-21 18:23:37 +0800

    编写插件

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import {Compiler} from 'webpack'
+import {Compiler, Stats} from 'webpack'
 import path from 'path'
 import fs from 'fs-extra'
 
-class WebpackPluginDeleteSourcemap {
+class MoveToPublicPathWebpackPlugin {
   apply(compiler:Compiler){
-    compiler.hooks.done.tap('move-to-public-path-webpack-plugin',(stats)=>{
-    
+    compiler.hooks.done.tap('move-to-public-path-webpack-plugin',(stats:Stats)=>{
+      const {compilation} = stats
+      if(!(typeof compilation.outputOptions.publicPath==='string')||compilation.outputOptions.publicPath==='auto')return;
+      const publicPath = /^[/]/.test(compilation.outputOptions.publicPath)?'.'+compilation.outputOptions.publicPath:compilation.outputOptions.publicPath
+      const outputPath = compilation.outputOptions.path
+      const oldPath = path.resolve(outputPath)
+      const newPath = path.resolve(outputPath,publicPath)
+      if(oldPath===newPath) return;
+      Object.keys(compilation.assets).filter((filename)=>(/[^index.html]/.test(filename))).map(async (filename)=>{
+        await fs.move(path.resolve(oldPath,filename),path.resolve(newPath,filename),{overwrite:true})
+      })
     })
   }
 }
-export default WebpackPluginDeleteSourcemap
+export default MoveToPublicPathWebpackPlugin
